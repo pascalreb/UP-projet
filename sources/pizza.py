7c3f3a65bace445bd50af313c27a51195e858562
Pascal Reb <pascal.reb10@gmail.com>
2023-02-03 11:00:38 +0100

    New pizza

--- sources/pizza.py
+++ sources/pizza.py
@@ -35,5 +35,9 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
+    },
+    'mypizza': {
+        'ingredients': {'mozzarella': 3, 'garlic': 5},
+        'price': 11,
     }
 }
